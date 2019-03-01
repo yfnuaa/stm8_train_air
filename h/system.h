240139a39ba1eb08495d59359412efc11925d924
Your Name <you@example.com>
2019-03-01 16:22:01 +0800

    “的”

--- h/system.h
+++ h/system.h
@@ -9,19 +9,16 @@
 
 #ifndef __SYSTEM_H___
 #define __SYSTEM_H___
-//#define USE_DEFAULT_CLK_2M
-#define DEBUG   1
-#define RUIZHU_TEST
+//#define USE_DEFAULT_CLK_2M    //open this to enable system to 2M default clock
+#define DEBUG   1               //enable this will send uart message 
+#define RUIZHU_TEST             //this is use for ruizhu test ,only funciton is adjust fan speed   0% 5% 10% 15% ... 80%
 enum system_mode
 {
     e_auto_mode = 0,
-    e_manual_mode,
-    e_sleep_mode,
+    e_manual_mode  ,
+    e_sleep_mode   ,
     e_power_off_mode
 };
-
  
 extern volatile enum system_mode g_system_mode;
- 
-
 #endif
